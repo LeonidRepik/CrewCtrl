9e86d0c8a91667c70ccee2c52cf8d3378fa8caec
Leo Repik <repiklleonid@gmail.com>
2024-08-19 18:53:35 -0700

    build with vercel + change node config and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+  root: './', // Set the root to the current directory (frontend)
+  build: {
+    outDir: '../dist', // Output directory relative to the root
+    rollupOptions: {
+      input: {
+        main: './index.html', // Entry file relative to the root directory
       },
     },
   },
