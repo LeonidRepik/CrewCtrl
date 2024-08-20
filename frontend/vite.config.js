bc60f900df0dff3d52f098ab4cd96cbed50816a6
Leo Repik <repiklleonid@gmail.com>
2024-08-19 19:28:18 -0700

    Just a few changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
-export default defineConfig({
-  root: './', // Set the root to the current directory (frontend)
-  build: {
-    outDir: '../dist', // Output directory relative to the root
-    rollupOptions: {
-      input: {
-        main: './index.html', // Entry file relative to the root directory
-      },
-    },
-  },
-});
+// export default defineConfig({
+//   root: './', // Set the root to the current directory (frontend)
+//   build: {
+//     outDir: '../dist', // Output directory relative to the root
+//     rollupOptions: {
+//       input: {
+//         main: './index.html', // Entry file relative to the root directory
+//       },
+//     },
+//   },
+// });
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 
@@ -27,3 +27,9 @@ export default defineConfig({
 //     },
 //   },
 // });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
