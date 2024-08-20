0d2679e084730f58c80be3ac094e30378a76ccdf
Leo Repik <repiklleonid@gmail.com>
2024-08-19 22:11:05 -0700

    dsa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-//vitejs.dev/config
+// vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -25,14 +25,14 @@ export default defineConfig({
         secure: false,
         configure: (proxy, _options) => {
           proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
+            console.log('Proxy error:', err);
           });
           proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
+            console.log('Sending Request to Target:', req.method, req.url);
           });
           proxy.on('proxyRes', (proxyRes, req, _res) => {
             console.log(
-              'Received Response from the Target:',
+              'Received Response from Target:',
               proxyRes.statusCode,
               req.url
             );
