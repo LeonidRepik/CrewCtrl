e9056ae066ec2dc2939a22508c1784a0f7287bbd
Leo Repik <repiklleonid@gmail.com>
2024-08-20 17:35:46 -0700

    ss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,30 +11,30 @@
 //     },
 //   },
 // });
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8000,
-    host: '127.0.0.1',
+    host: "127.0.0.1",
     proxy: {
-      '/api': {
-        target: 'https://crewctrl-29196d49230a.herokuapp.com',
+      "/api": {
+        target: "https://crewctrl-29196d49230a.herokuapp.com/api",
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('Proxy error:', err);
+          proxy.on("error", (err, _req, _res) => {
+            console.log("Proxy error:", err);
           });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to Target:', req.method, req.url);
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to Target:", req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
             console.log(
-              'Received Response from Target:',
+              "Received Response from Target:",
               proxyRes.statusCode,
               req.url
             );
