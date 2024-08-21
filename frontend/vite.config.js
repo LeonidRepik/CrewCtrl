4a0f70c99b4f45b5820eefc405273e884305cc6d
Leo Repik <repiklleonid@gmail.com>
2024-08-20 18:33:43 -0700

    ss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -102,19 +102,32 @@
 //   },
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://crewctrl-29196d49230a.herokuapp.com/api",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//         secure: false, // HTTPS
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://crewctrl-29196d49230a.herokuapp.com/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false, // HTTPS
-      },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "/index.html",
     },
   },
 });
